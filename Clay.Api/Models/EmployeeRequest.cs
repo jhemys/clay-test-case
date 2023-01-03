a58df283a413116d2bf251167316cb7f31e7f780
Jhemys Alves <jhemys@outlook.com>
2023-01-03 18:43:45 -0300

    Adjusting models for Employees

--- Clay.Api/Models/EmployeeRequest.cs
+++ Clay.Api/Models/EmployeeRequest.cs
@@ -7,8 +7,6 @@ namespace Clay.Api.Models
     {
         public string Name { get; set; }
         public string Role { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
         public string? TagIdentification { get; set; }
     }
 }
--- Clay.Api/Models/EmployeeResponse.cs
+++ Clay.Api/Models/EmployeeResponse.cs
@@ -8,7 +8,6 @@ namespace Clay.Api.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Role { get; set; }
-        public string Email { get; set; }
         public string TagIdentification { get; set; }
         public bool IsActive { get; set; }
     }
